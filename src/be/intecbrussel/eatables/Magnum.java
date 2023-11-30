3da316205175ea46e2ddff5f31487c73562d5571
Janan Javdan <Fatemeh.Javdan@student.intecbrussel.be>
2023-11-30 15:05:11 +0100

    Add files via upload

---
+++ src/be/intecbrussel/eatables/Magnum.java
@@ -0,0 +1,36 @@
+package be.intecbrussel.eatables;
+
+import static be.intecbrussel.eatables.Magnum.*;
+
+public class Magnum implements Eatables {
+
+    public MagnumType type;
+
+    public Magnum(MagnumType type) {
+        this.type = type;
+    }
+
+    public MagnumType getType() {
+        return type;
+    }
+
+    public void setType(MagnumType type) {
+        this.type = type;
+    }
+
+
+        @Override
+        public void eat() {
+            System.out.println("je eet Magnum Ice Cream Van Het Type "+type.toString()+ " "+"Magnum");
+        }
+
+    //De eat methode print af wat je aan het eten bent
+    public enum MagnumType {
+        ALPINENUTS,
+        BLACKCHOCOLATE,
+        MILKCHOCOLATE,
+        ROMANTICSTRAWBERRIES,
+        WHITECHOCOLATE;
+    }
+}
+
